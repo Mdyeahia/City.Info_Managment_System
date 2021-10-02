589793797277f12efe2af1b2b3fd005b92223fa2
Mdyeahia <mdyeahia.khan@gmail.com>
2021-10-02 23:06:57 +0600

    Add project files.

---
+++ City.IMS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(City.IMS.Startup))]
+namespace City.IMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
